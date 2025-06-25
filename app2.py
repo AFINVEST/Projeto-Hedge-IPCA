6d94a4f5289b9e14f2f369175a1793a6fd21abe3
Emanuel Gandra <emanuelgandra00@gmail.com>
2025-06-25 10:21:49 -0300

    DashRisco V100

--- app2.py
+++ app2.py
@@ -124,7 +124,7 @@ def load_carteira_hoje() -> pd.DataFrame:
     #                         "ativo":"Ativo",
     #                         "estrategia":"Estratégia",
     #                         "quantidade":"Quantidade"}))
-    df = pd.read_excel('Dados/Relatório de Posição 2025-06-10.xlsx')
+    df = pd.read_excel('Dados/Relatório de Posição 2025-06-23.xlsx')
 
     # se precisar de 'Valor' em algum ponto mais à frente:
     if "Valor" not in df.columns:
