a242edc96e80293526ee5bd06aac7eb77eac134c
Emanuel Gandra <emanuelgandra00@gmail.com>
2025-10-28 15:52:49 -0300

    DashHedge att 28/10

--- app2.py
+++ app2.py
@@ -135,7 +135,7 @@ def load_carteira_hoje() -> pd.DataFrame:
     #                         "ativo":"Ativo",
     #                         "estrategia":"Estratégia",
     #                         "quantidade":"Quantidade"}))
-    df = pd.read_excel('Dados/Relatório de Posição 2025-10-22.xlsx')
+    df = pd.read_excel('Dados/Relatório de Posição 2025-10-24.xlsx')
 
     # se precisar de 'Valor' em algum ponto mais à frente:
     if "Valor" not in df.columns:
