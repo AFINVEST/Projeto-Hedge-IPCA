1a9d4a67dbec6ce9f7163d9a20587de4d468b057
Emanuel Gandra <emanuelgandra00@gmail.com>
2025-11-06 19:20:02 -0300

    DashHedge

--- excecoes_tratamento.py
+++ excecoes_tratamento.py
@@ -73,11 +73,11 @@ ASSETS: list[dict] = [
          url="https://afinvest.com.br/interno/relatorios/detalhes-de-ativos?id=3417&codativo=CERT11"),
 
     # --------- ATIVOS CASUAIS (taxa informada manualmente) ----------------- (Manter ordem de DEB, CRI, CRA)
-    dict(code_xp="ENMTB3", tab="DEB",
-         label="ENMTB3", rate_src="manual", rate=7.3204),
+    #dict(code_xp="ENMTB3", tab="DEB",
+    #     label="ENMTB3", rate_src="manual", rate=7.3204),
 
-    dict(code_xp="CPLD37", tab="DEB",
-         label="CPLD37", rate_src="manual", rate=7.0728),
+    #dict(code_xp="CPLD37", tab="DEB",
+    #     label="CPLD37", rate_src="manual", rate=7.0728),
 
     # dict(code_xp ="ECHP11", tab="DEB",
     #     label="ECHP11", rate_src="manual", rate=8.214500),
